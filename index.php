ff4a45ad01ec3d2deda4417eb249cf164aa06e0e
Hugo Froes <hugofroesdesign@gmail.com>
2018-11-27 16:27:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,34 @@
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
   </head>
   <body>
+    <header>
+      <section class="hero is-fullheight is-default is-bold">
+        <div class="hero-head">
+            <nav class="navbar">
+              <div class="container">
+                <div class="navbar-brand">
+                  <a class="navbar-item" href="../">
+                    <img src="../images/bulma.png" alt="Logo">
+                  </a>
+                  <div id="navbarMenu" class="navbar-menu">
+                    <div class="navbar-end">
+                      <div class="tabs is-right">
+                        <ul>
+                          <li class="is-active"><a>Home</a></li>
+                          <li><a href="">Examples</a></li>
+                          <li><a href="">Features</a></li>
+                          <li><a href="">Team</a></li>
+                          <li><a href="">Help</a></li>
+                        </ul>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </nav>
+        </div>
+      </section>
+    </header>
+
   <section class="section">
     <div class="container">
       <h1 class="title">
