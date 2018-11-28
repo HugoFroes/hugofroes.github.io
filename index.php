0565faa8304aa9cf280e3df7ce9c8d473b2725d6
Hugo Froes <hugofroesdesign@gmail.com>
2018-11-28 15:25:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -35,6 +35,15 @@
         </div>
       </section>
     </header>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hugo Froes - Boilerplate</title>
+	<link rel="stylesheet" href="css/style.css">
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
 
   <section class="section">
     <div class="container">
@@ -47,4 +56,9 @@
     </div>
   </section>
   </body>
+<body id="home">
+
+	<h1>Boilerplate layout</h1>
+
+</body>
 </html>
