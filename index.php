c51dcd9d2f020fb12afa80500b71209a37f5f43b
Hugo Froes <hugofroesdesign@gmail.com>
2018-11-28 15:28:41 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,4 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
-  </head>
-  <body>
-    <header>
-      <section class="hero is-fullheight is-default is-bold">
-        <div class="hero-head">
-            <nav class="navbar">
-              <div class="container">
-                <div class="navbar-brand">
-                  <a class="navbar-item" href="../">
-                    <img src="../images/bulma.png" alt="Logo">
-                  </a>
-                  <div id="navbarMenu" class="navbar-menu">
-                    <div class="navbar-end">
-                      <div class="tabs is-right">
-                        <ul>
-                          <li class="is-active"><a>Home</a></li>
-                          <li><a href="">Examples</a></li>
-                          <li><a href="">Features</a></li>
-                          <li><a href="">Team</a></li>
-                          <li><a href="">Help</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                </div>
-              </div>
-            </nav>
-        </div>
-      </section>
-    </header>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
@@ -45,17 +9,6 @@
 	<![endif]-->
 </head>
 
-  <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
-    </div>
-  </section>
-  </body>
 <body id="home">
 
 	<h1>Boilerplate layout</h1>
