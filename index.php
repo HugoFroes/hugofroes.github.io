7f96e70ec270eae2816d4aeddf3e055d23f34065
Hugo Froes <hugofroesdesign@gmail.com>
2018-12-04 11:50:49 +0000

    Worked on Header

--- index.php
+++ index.php
@@ -2,16 +2,177 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Hugo Froes - Boilerplate</title>
-	<link rel="stylesheet" href="css/style.css">
+	<title>Site boilerplate</title>
+	<link rel="stylesheet" href="/scripts/base.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato|Noto+Sans+SC:700" rel="stylesheet">
 	<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 </head>
 
 <body id="home">
+  <header>
+		<div class="container">
+	    <a href="#" class="logo_header">
+				<img class="header_icon" src="/images/ideas.svg" alt="Light bulb over a box">
+				<h1>Company name</h1>
+			</a>
+	    <nav>
+	      <ul>
+	        <li><a href="#">Home</a></li>
+	        <li><a href="#">Work</a></li>
+	        <li><a href="#">Agenda</a></li>
+	        <li><a href="#">Resources</a></li>
+	        <li><a href="#">About</a></li>
+	        <li><a href="#">Talk to me</a></li>
+	      </ul>
+	    </nav>
+		</div>
+  </header>
 
-	<h1>Boilerplate layout</h1>
+  <!-- <section class="hero">
+		<div class="container">
+    	<h2>Solving problems with a Human Centred Design approach</h2>
+			<div class="image-placeholder">
+		    <p>Really cool hero image here</p>
+			</div>
+		</div>
+  </section>
+
+  <section>
+		<div class="container">
+	    <h3>Expertise</h3>
+	    <p>I've worked with various teams in both enterprise and startup environment. I am versed in various human centered approaches to solution devlopment such as Agile, Scrum, Lean UX, Design Sprints, Design Thinking and Service Design. Below are a list of my general expertise.</p>
+	    <div>
+	      <article>
+	        <h4>Research</h4>
+	        <p>Description</p>
+	        <ul>
+	          <li>Observation</li>
+	          <li>Interview</li>
+	          <li>Survey</li>
+	          <li>Workshops</li>
+	          <li>...</li>
+	        </ul>
+	      </article>
+			</div>
+			<div>
+	      <article>
+					<h4>Ideation</h4>
+					<p>Description</p>
+					<ul>
+						<li>Wireframing</li>
+						<li>Mapping</li>
+						<li>Design systems</li>
+						<li>Workshops</li>
+						<li>...</li>
+					</ul>
+	      </article>
+			</div>
+			<div>
+	      <article>
+					<h4>Ideation</h4>
+					<p>Description</p>
+					<ul>
+						<li>Wireframing</li>
+						<li>Mapping</li>
+						<li>Design systems</li>
+						<li>Workshops</li>
+						<li>...</li>
+					</ul>
+	      </article>
+			</div>
+			<div>
+	      <article>
+					<h3>Testing</h3>
+					<p>Description</p>
+					<ul>
+						<li>Contextual</li>
+						<li>Unmoderated/Remote</li>
+						<li>A/B testing</li>
+						<li>Usability</li>
+						<li>...</li>
+					</ul>
+	      </article>
+			</div>
+		</div>
+  </section>
+
+	<section>
+		<div class="container">
+			<h3>Latest articles from Medium</h3>
+			<article>
+				<img class="card-img-top" src="https://miro.medium.com/fit/c/1400/420/1*fS1OZ9zN_Q4UXWNYVlUuDA.jpeg">
+				<div class="card-body">
+					<h4>Why we need to stop over-complicating UX</h4>
+					<p class="card-text">You’ve just finished an incredible course where the instructor has given you a huge range of tools and methodologies to put towards generating incredible experiences for you.</p>
+					<button type="button" class="btn btn-primary">Keep reading</button>
+				</div>
+			</article>
+			<article>
+				<img class="card-img-top" src="https://miro.medium.com/fit/c/1400/420/1*fS1OZ9zN_Q4UXWNYVlUuDA.jpeg">
+				<div class="card-body">
+					<h4>Why we need to stop over-complicating UX</h4>
+					<p class="card-text">You’ve just finished an incredible course where the instructor has given you a huge range of tools and methodologies to put towards generating incredible experiences for you.</p>
+					<button type="button" class="btn btn-primary">Keep reading</button>
+				</div>
+			</article>
+			<article>
+				<img class="card-img-top" src="https://miro.medium.com/fit/c/1400/420/1*fS1OZ9zN_Q4UXWNYVlUuDA.jpeg">
+				<div class="card-body">
+					<h4>Why we need to stop over-complicating UX</h4>
+					<p class="card-text">You’ve just finished an incredible course where the instructor has given you a huge range of tools and methodologies to put towards generating incredible experiences for you.</p>
+					<button type="button" class="btn btn-primary">Keep reading</button>
+				</div>
+			</article>
+			<article>
+				<img class="card-img-top" src="https://miro.medium.com/fit/c/1400/420/1*fS1OZ9zN_Q4UXWNYVlUuDA.jpeg">
+				<div class="card-body">
+					<h4>Why we need to stop over-complicating UX</h4>
+					<p class="card-text">You’ve just finished an incredible course where the instructor has given you a huge range of tools and methodologies to put towards generating incredible experiences for you.</p>
+					<button type="button" class="btn btn-primary">Keep reading</button>
+				</div>
+			</article>
+		</div>
+	</section>
+
+	<section>
+		<div class="container">
+			<h2>Testimonials</h2>
+			<article>
+				<img>
+				<h3>Name <span>Postition | Company</span></h3>
+				<p>Testominial</p>
+			</article>
+			<article>
+				<img>
+				<h3>Name <span>Postition | Company</span></h3>
+				<p>Testominial</p>
+			</article>
+			<article>
+				<img>
+				<h3>Name <span>Postition | Company</span></h3>
+				<p>Testominial</p>
+			</article>
+			<article>
+				<img>
+				<h3>Name <span>Postition | Company</span></h3>
+				<p>Testominial</p>
+			</article>
+			<article>
+				<img>
+				<h3>Name <span>Postition | Company</span></h3>
+				<p>Testominial</p>
+			</article>
+		</div>
+	</section>
+
+  <footer>
+		<div class="container">
+			<p>© Copyright 2018 - Hugo Froes</p>
+			<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+		</div>
+  </footer> -->
 
 </body>
 </html>
