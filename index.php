4501a58b37a313cfc534102d29afa15daaa54d9d
Hugo Froes <hugofroesdesign@gmail.com>
2018-12-17 11:46:09 +0000

    Update homepage
    An update to the homepage using boostrap framework for easier handling of classes and responsive layouts

--- index.php
+++ index.php
@@ -1,36 +1,167 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Site boilerplate</title>
-	<link rel="stylesheet" href="/scripts/base.css">
-	<link href="https://fonts.googleapis.com/css?family=Lato|Noto+Sans+SC:700" rel="stylesheet">
-	<!--[if IE]>
-		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-</head>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<body id="home">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/scripts/base.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+		<!-- HEADER -->
+		<header class="navbar navbar-expand-lg navbar-light bg-light flex-column flex-md-row bd-navbar">
+			<a class="navbar-brand mr-0 mr-md-2">Company Name</a>
+			<ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Work</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Agenda</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Resources</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Talk to me</a>
+				</li>
+			</ul>
+		</header>
+		<!-- HERO SECTION -->
+		<section class="jumbotron jumbotron-fluid">
+			<div class="container">
+				<div class="row">
+					<h1 class="col-sm display-3">Solving problems with a Human Centred Design approach</h1>
+					<div class="image-placeholder col-sm">
+			    	<p>Really cool hero image here</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="py-5">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm">
+						<img src="..." class="img-fluid" alt="Responsive image">
+					</div>
+					<div class="col-sm">
+						<h2>Hugo Froes</h2>
+						<p class="lead">Paragraph of text goes here</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- ARTICLES -->
+		<section class=" py-5">
+			<div class="container">
+				<div class="row">
+						<h2>Articles on Medium</h2>
+				</div>
+				<div class="row">
+					<article class="col-4">
+						<div class="card">
+							<img class="card-img-top" src="https://miro.medium.com/fit/c/1400/420/1*fS1OZ9zN_Q4UXWNYVlUuDA.jpeg" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+	    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	    					<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</article>
+					<article class="col-4">
+						<div class="card">
+							<img class="card-img-top" src="https://miro.medium.com/fit/c/1400/420/1*fS1OZ9zN_Q4UXWNYVlUuDA.jpeg" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+	    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	    					<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</article>
+					<article class="col-4">
+						<div class="card">
+							<img class="card-img-top" src="https://miro.medium.com/fit/c/1400/420/1*fS1OZ9zN_Q4UXWNYVlUuDA.jpeg" alt="Card image cap">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+	    					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	    					<a href="#" class="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</article>
+				</div>
+			</div>
+		</section>
+
+		<!-- Testimonials -->
+		<section>
+			<div class="container">
+				<div class="row">
+					<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+					  <li class="nav-item">
+					    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</a>
+					  </li>
+					</ul>
+					<div class="tab-content" id="pills-tabContent">
+					  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
+					  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+					  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- FOOTER -->
+
+		<footer class="bg-dark">
+			<div class="container pt-3">
+				<div class="row">
+					<p class="col-sm text-white bg-dark text-center">© Copyright 2018 - Hugo Froes</p>
+				</div>
+			</div>
+		</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<!--<body id="home">
   <header>
 		<div class="container">
 	    <a href="#" class="logo_header">
-				<img class="header_icon" src="/images/ideas.svg" alt="Light bulb over a box">
 				<h1>Company name</h1>
 			</a>
 	    <nav>
 	      <ul>
-	        <li><a href="#">Home</a></li>
-	        <li><a href="#">Work</a></li>
-	        <li><a href="#">Agenda</a></li>
-	        <li><a href="#">Resources</a></li>
-	        <li><a href="#">About</a></li>
-	        <li><a href="#">Talk to me</a></li>
+	        <li><a href="#" class="home">Home</a></li>
+	        <li><a href="#" class="work">Work</a></li>
+	        <li><a href="#" class="agenda">Agenda</a></li>
+	        <li><a href="#" class="resources">Resources</a></li>
+	        <li><a href="#" class="about">About</a></li>
+	        <li><a href="#" class="talk">Talk to me</a></li>
 	      </ul>
 	    </nav>
 		</div>
   </header>
 
-  <!-- <section class="hero">
+  <section class="hero">
 		<div class="container">
     	<h2>Solving problems with a Human Centred Design approach</h2>
 			<div class="image-placeholder">
@@ -173,6 +304,3 @@
 			<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 		</div>
   </footer> -->
-
-</body>
-</html>
